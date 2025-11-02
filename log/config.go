a1f8c68c0a447a2e26ce03248fec7493f05fe76c
Denis Beresnev <shanthi.bunch@gmail.com>
2025-11-03 00:24:12 +0300

    feat: updated configs

--- log/config.go
+++ log/config.go
@@ -1,10 +1,10 @@
 package log
 
 type Config struct {
-	Level        string `yaml:"level"`
-	App          string `yaml:"app"`
-	Service      string `yaml:"service"`
-	EnableCaller bool   `yaml:"enable_caller"`
-	UDPAddress   string `yaml:"udp_address"`
-	Console      bool   `yaml:"console"`
+	Level        string `mapstructure:"level" yaml:"level" json:"level" toml:"level"`
+	App          string `mapstructure:"app" yaml:"app" json:"app" toml:"app"`
+	Service      string `mapstructure:"service" yaml:"service" json:"service" toml:"service"`
+	EnableCaller bool   `mapstructure:"enable_caller" yaml:"enable_caller" json:"enable_caller" toml:"enable_caller"`
+	UDPAddress   string `mapstructure:"udp_address" yaml:"udp_address" json:"udp_address" toml:"udp_address"`
+	Console      bool   `mapstructure:"console" yaml:"console" json:"console" toml:"console"`
 }
